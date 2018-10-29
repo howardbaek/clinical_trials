651beda1d5c2a960b8589c90fdc21034944f9b18
Jason Baik <jasonbaik@Jasons-MacBook-Pro.local>
2018-10-29 18:09:57 -0400

    Convert xml to dataframe

--- clinicaltrials_govdata_download.R
+++ clinicaltrials_govdata_download.R
@@ -9,7 +9,8 @@
 #' source ~/Scripts/bash/submit_Rjob.sh download_clinicaltrials_govdata.R
 
 
-data_dir <- "../../data/"
+# First download ----------------------------------------------------------
+data_dir <- "../data/"
 out_dir <- paste0(data_dir,"ClinicalTrialsdotGov/")
 
 destfile <- paste0(out_dir,"AllPublicXML.zip")
@@ -19,8 +20,24 @@ download.file("https://clinicaltrials.gov/AllPublicXML.zip",
 
 unzip(destfile,exdir = out_dir)
 
+
+# Second download ---------------------------------------------------------
 destfile <- paste0(out_dir,"AllPublicXML_schema.xsd")
 download.file("https://clinicaltrials.gov/ct2/html/images/info/public.xsd",
               destfile=destfile)
 
 
+
+# Test reading xml file into dataframe ------------------------------------
+library(XML)
+
+xmlfile <- xmlTreeParse("./data/NCT00000102.xml")
+class(xmlfile)
+topxml <- xmlRoot(xmlfile)
+
+topxml <- xmlSApply(topxml,
+                    function(x) xmlSApply(x, xmlValue))
+
+xml_df <- data.frame(t(topxml), row.names=NULL)
+
+#test <- xmlToDataFrame("./data/NCT00000102.xml")
